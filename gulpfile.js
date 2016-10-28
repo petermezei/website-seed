2045464f474cf81e105fe7752c913db11e4dfbc4
Peter Mezei <peter.mezei@slamby.com>
2016-10-28 16:05:50 +0200

    Init2

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var bs = require('browser-sync').create();
+
+gulp.task('browser-sync', ['sass'], function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('sass', function () {
+    return gulp.src('scss/*.scss')
+                .pipe(sass())
+                .pipe(gulp.dest('css'))
+                .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', bs.reload);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch', 'browser-sync', 'watch']);
